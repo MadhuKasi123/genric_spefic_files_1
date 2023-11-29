e2d7a79df87241f30f7a758c6d17c7b3a81ad9c0
MadhuKasi123 <madhuroyal070@gmail.com>
2023-11-29 18:44:35 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def madhu(request):
+    return render(request,'madhu.html')
+
+def kasi(request):
+    return render(request,'kasi.html')
